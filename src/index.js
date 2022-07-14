8160c0b603ac751ffb755adead1effb0df990121
Tarek <83262890+tarek102@users.noreply.github.com>
2022-07-14 13:14:55 +0200

    Add font awesome, style the to-do-list, create array with tasks

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 import _ from 'lodash';
 import './style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
-function component() {
+  const toDoList = document.querySelector('.to-do-list');
   let listItems = [
     {
-      description: "Hello",
-      completed: true,
+      description: "Feed the dog",
+      completed: false,
+      index: 0
+    },
+    {
+      description: "Make breakfast",
+      completed: false,
+      index: 0
+    },
+    {
+      description: "Get grocery",
+      completed: false,
       index: 0
     }
   ];
-  
-}
 
-document.body.appendChild(component());
+  listItems.forEach((item) => {
+    const newItem = document.createElement('li');
+    newItem.innerHTML = 
+    `
+    <div>
+    <input type="checkbox" class="items-list"> 
+    <label for="vehicle1"> ${item.description}</label><br>
+    </div>
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+    `;
+  
+    toDoList.appendChild(newItem);
+  });
+
+
