d20655a5a2b735e5fb5474713f9f4fbd2091cf18
Tarek <83262890+tarek102@users.noreply.github.com>
2022-07-14 13:31:35 +0200

    Add linters

--- src/index.js
+++ src/index.js
@@ -1,41 +1,37 @@
-import _ from 'lodash';
 import './style.css';
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
-  const toDoList = document.querySelector('.to-do-list');
-  let listItems = [
-    {
-      description: "Feed the dog",
-      completed: false,
-      index: 0
-    },
-    {
-      description: "Make breakfast",
-      completed: false,
-      index: 0
-    },
-    {
-      description: "Get grocery",
-      completed: false,
-      index: 0
-    }
-  ];
+const toDoList = document.querySelector('.to-do-list');
+const listItems = [
+  {
+    description: 'Feed the dog',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Make breakfast',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Get grocery',
+    completed: false,
+    index: 0,
+  },
+];
 
-  listItems.forEach((item) => {
-    const newItem = document.createElement('li');
-    newItem.innerHTML = 
-    `
+listItems.forEach((item) => {
+  const newItem = document.createElement('li');
+  newItem.innerHTML = `
     <div>
     <input type="checkbox" class="items-list"> 
     <label for="vehicle1"> ${item.description}</label><br>
     </div>
     <i class="fa-solid fa-ellipsis-vertical"></i>
     `;
-  
-    toDoList.appendChild(newItem);
-  });
 
-
+  toDoList.appendChild(newItem);
+});
