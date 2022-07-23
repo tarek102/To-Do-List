3cce745a3acd1d4330591163194f07a50728ed41
Tarek <83262890+tarek102@users.noreply.github.com>
2022-07-23 03:38:06 +0200

    update index.html

--- modules/Store.js
+++ modules/Store.js
@@ -32,6 +32,7 @@ export default class Store {
     return (filteredLists);
   }
 
+  // Edit/Update task 
   static editTask = (el) => {
     const Lists = this.getLists();
     Lists.forEach(list => {
